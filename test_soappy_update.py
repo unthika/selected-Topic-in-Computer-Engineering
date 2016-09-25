ea6613ce0169339ca6857ebdab3a58fbcafcc63b
unthika <com5630281@gmail.com>
2016-09-26 00:41:12 +0700

    Add files via upload

---
+++ test_soappy_update.py
@@ -0,0 +1,13 @@
+import sys
+from SOAPpy import SOAPProxy
+from SOAPpy import WSDL
+import xml.etree.ElementTree as ET
+serverUrl='http://localhost:8081'
+namespace='xml'
+server = SOAPProxy(serverUrl, namespace)
+#server.config.dumpSOAPOut = 1
+#server.config.dumpSOAPIn = 1
+server.update_xml("movie name",["Adventure","Sifi"],["name1","name2"],"name3","1","oct","1234")
+response = server.movieNameFromeType("Adventure")
+print response
+
